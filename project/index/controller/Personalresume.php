d952fd953e8d3883629deda6fb8e23bd99b8b971
yangfei <yangfei.eng@gmail.com>
2019-07-01 21:20:55 +0800

    后台简历管理-分页、查询、审核

--- project/index/controller/Personalresume.php
+++ project/index/controller/Personalresume.php
@@ -15,74 +15,57 @@ class Personalresume extends Common {
     //默认入口
     public function index(){
         $get = input('get.');
-        $where = [];
-        $where['pt.isdel'] = ['neq',1];
+        $where = ['isdel' => 0];
 
-        if(!empty($get['companyName'])){
-            $where['cm.name'] = ['like', '%'.$get['companyName'].'%'];
+        if(!empty($get['personname'])){
+            $where['personname'] = ['like', '%'.$get['personname'].'%'];
+        }
+        if(!empty($get['telephone'])){
+            $where['telephone'] = ['like', '%'.$get['telephone'].'%'];
+        }
+        if(!empty($get['email'])){
+            $where['email'] = ['like', '%'.$get['email'].'%'];
+        }
+        if(!empty($get['auditstatus'])){
+            $where['auditstatus'] = ['eq',$get['auditstatus']];
         }
 
-        $countPage = $this->model->getCount($where);
-        $this->assign('countPage',$countPage);
-        return view('Personalposition/index');
+        $countPage = $this -> model -> getCount($where);
+        $this -> assign('countPage',$countPage);
+        return view('Personalresume/index');
     }
 
-		//新增
-	public function add(){
-		if($this->request->isPost()){
-			$flag = $this->model->add($this->request);
-			if($flag){
-				$this->success('添加成功', url('lists'));
-			}else{
-				$this->error('添加失败');
-			}
-		}else{
-			return $this->fetch();
-		}
-	}
-		//修改
-	public function edit(){
-		if($this->request->isPost()){
-			$flag = $this->model->edit($this->request);
-			if($flag){
-				$this->success('编辑成功', url('lists'));
-			}else{
-				$this->error('编辑失败');
-			}
-		}else{
-			$personal_resume = $this->model->info($this->request);
-			$this->assign('personal_resume', $personal_resume);
-			return $this->fetch();
-		}
-	}
-		//删除
-	public function delete(){
-		$flag = $this->model->del($this->request);
-		if($flag){
-			$this->success('删除成功', url('lists'));
-		}else{
-			$this->error('删除失败');
-		}
-	}
-	    //批量删除
-    public function delList(){
-        $flag = $this->model->delList($this->request);
-        if($flag){
-			$this->success('删除成功', url('lists'));
-		}else{
-			$this->error('删除失败');
-		}
+    //获取列表
+    public function getList(){
+        $get = input('get.');
+        $page = empty($get['page'])?1:$get['page'];
+        $count = empty($get['count'])?10:$get['count'];
+
+        $where = ['isdel' => 0];
+
+        if(!empty($get['personname'])){
+            $where['personname'] = ['like', '%'.$get['personname'].'%'];
+        }
+        if(!empty($get['telephone'])){
+            $where['telephone'] = ['like', '%'.$get['telephone'].'%'];
+        }
+        if(!empty($get['email'])){
+            $where['email'] = ['like', '%'.$get['email'].'%'];
+        }
+        if(!empty($get['auditstatus'])){
+            $where['auditstatus'] = ['eq',$get['auditstatus']];
+        }
+
+        $data = $this -> model -> getPageData($page,$count,$where);
+
+        return json(['data'=>$data,'status'=>1,'msg'=>'获取数据成功']);
     }
-	    //id单个查询
-    public function info(){
-        $personal_resume = $this->model->info($this->request);
-        $this->assign('personal_resume', $personal_resume);
-        return $this->fetch();
+
+    public function auditData(){
+        if(request()->isPost()){ $id = input('post.id'); }else{ return json(['status'=>-1,'msg'=>'未知数据']); }
+
+        $return = $this->model->auditDataById($id);
+
+        return $return ? ['status'=>1,'msg'=>'审核通过'] : ['status'=>-1,'msg'=>'审核失败'];
     }
-		//列表
-	public function lists(){
-		$personal_resumeList = $this->model->lists($this->request, 12);	
-		$this->assign('personal_resumeList', $personal_resumeList);
-		return $this->fetch();
-	}
 }
--- project/index/model/PersonalResume.php
+++ project/index/model/PersonalResume.php
@@ -1,48 +1,21 @@
 <?php
 namespace app\index\model;
 
-use think\Model;
+class PersonalResume extends Common {
 
-class PersonalResume extends Model {
-		    //新增
-    public function add($request){
-        $data = $request->param();
-        foreach($data as $key=>$val){
-            if(is_array($val)){    //处理checkbox情况
-                $data[$key] = implode("#op#", $val);
-            }
-        }
-        return $this->data($data)->allowField(true)->save();
-    }
-	    //修改
-    public function edit($request){
-        $data = $request->param();
-        foreach($data as $key=>$val){
-            if(is_array($val)){    //处理checkbox情况
-                $data[$key] = implode("#op#", $val);
-            }
-        }
-        return $this->allowField(true)->save($data, ['id' => $data['id']]);
-    }
-	    //删除
-    public function del($request){
-        $id = $request->param('id');
-        return $this->where('id',  $id)->delete();
-    }
-	    //批量删除
-    public function delList($request){
-        $condition = $request->request('condition');
-        return $this->destroy(json_decode($condition));
-    }
-	    //id单个查询
-    public function info($request){
-        $id = $request->param('id');		
-        return $this->where('id', $id)->find();
-    }
-	    //列表
-    public function lists($request, $itemNum = 12){	//每页显示12条数据
-        $condition = $request->param('condition');
-        return $this->where(json_decode($condition))->paginate($itemNum);
+    //分页数据
+    public function getPageData($page = 1,$count = 10,$where=[]){
+        $data = $this::field('id,personname,birthday,education,jobstatus,expectregion,address,telephone,email,auditstatus,workexperience,jointime') -> where($where) -> page($page.','.$count) -> select();
+        return $data;
     }
 
+    //获取总条数
+    public function getCount($where = []){
+        return $this -> where($where) -> count();
+    }
+
+    //审核数据
+    public function  auditDataById($id){
+        return $this::update(['id'=>$id,'auditstatus'=>2]);
+    }
 }	
