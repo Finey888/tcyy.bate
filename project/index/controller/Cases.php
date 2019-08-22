38c577e4010c635fb56a359f6a420887b383030f
yangfei <271981228@qq.com>
2019-08-22 18:01:55 +0800

    后台系统增加病例秀功能模块

---
+++ project/index/controller/Cases.php
@@ -0,0 +1,70 @@
+<?php
+namespace app\index\controller;
+use think\Request;
+
+class Cases extends Common {
+
+    private $caseModel;
+
+    public function __construct(Request $request = null,$options = [])
+    {
+        parent::__construct($request);
+        $this-> caseModel = new \app\index\model\Cases();
+        $this-> groupModel = new \app\index\model\Group();
+    }
+
+    //默认入口
+    public function index(){
+        $get = input('get.');
+
+        $group = $this->groupModel->getAllData();
+        $this->assign('group', $group);
+
+        $where ='';
+
+        if(isset($get['group_id']) && $get['group_id'] != 0){
+            $where = ['group_id'=>$get['group_id']];
+        }
+        $countPage = $this->caseModel->getCount($where);
+        $this->assign('countPage',$countPage);
+        return view('Cases/index');
+    }
+
+    //获取详情
+    public function details(){
+        $get = input('get.');
+
+        $data = $this->caseModel->getCasesDetails($get['id']);
+        $return['contents'] = $data['contents'];
+        $return['user_info'] = $data['user_info'];
+        $return['thing'] = $data['thing'];
+        $return['read'] = $data['read'];
+        $return['comments'] = $data['comments'];
+        $return['creatime']= date('Y-m-d',$data['creatime']);
+        if(empty($data['cases_image'])){
+            $return['cases_image']=[];
+        }else{
+            $return['cases_image']=$data['cases_image']->toArray();
+        };
+        $this->assign('data',$return);
+        return view('Cases/details');
+    }
+
+    public function getList(){
+        $get = input('get.');
+        $where ='';
+        if(isset($get['group_id']) && $get['group_id'] != 0){
+            $where = ['group_id'=>$get['group_id']];
+        }
+        $page = empty($get['page'])?1:$get['page'];
+        $limit = empty($get['limit'])?10:$get['limit'];
+
+        $data = $this->caseModel->getDataByPage($where,$page,$limit);
+
+        return json(['data'=>$data,'status'=>1,'msg'=>'获取数据成功']);
+
+    }
+
+}
+
+
---
+++ project/index/model/Cases.php
@@ -0,0 +1,96 @@
+<?php
+namespace app\index\model;
+
+class Cases extends Common{
+
+    //自定义初始化
+    protected function initialize()
+    {
+        //需要调用`Model`的`initialize`方法
+        parent::initialize();
+    }
+
+    public function UserInfo()
+    {
+        return $this->hasOne('UserInfo','uid','uid');
+    }
+
+    public function CasesImage()
+    {
+        return $this->hasMany('CasesImage','case_id');
+    }
+
+    public function getDataById($id){
+        return $this::where(['id'=>$id])->find();
+    }
+
+    //获取数据
+    public function getDataByPage($where=[],$page=1,$count=10){
+
+//        $where=[];
+//        if(($type==1)){
+//            $where = ['group_id'=>$group_id,'status'=>1,'type'=>$type];
+//        }elseif(($type==2)){
+//            $where = ['status'=>1,'type'=>$type,'uid'=>$uid];
+//        }elseif(($type==3)){
+//            $where = ['status'=>1,'type'=>1,'uid'=>$uid];
+//        }
+//
+//        $data = $this::with(['UserInfo'=>function($query){
+//            $query->field('id,uid,nickname,headurl');
+//        },'CasesFollow'=>function($query)use($uid){
+//            $query->where(['uid'=>$uid])->field('id,touid,status');
+//        },'CasesImage'=>function($query){
+//            $query->field('id,case_id,image');
+//        }])
+//            ->where($where)
+//            ->page($page.','.$count)
+//            ->order('creatime desc')
+//            ->select();
+
+        $data = $this::join('tcyy_user_info u', ' u.uid = tcyy_cases.uid ', 'left') ->where($where)
+            -> page($page.','.$count)
+            -> field('tcyy_cases.id,thing,read,comments,DATE_FORMAT(FROM_UNIXTIME(creatime),\'%Y-%m-%d\') as creatime,u.nickname')
+            -> order('creatime desc')
+            -> select();
+
+        return empty($data)?[]:$data->toArray();
+    }
+
+    public function getCount($where=[]){
+        return $data = $this::join('tcyy_user_info u', ' u.uid = tcyy_cases.uid ', 'left') ->where($where)
+            -> field('tcyy_cases.id,thing,read,comments,creatime,u.nickname') ->count();
+    }
+
+    public function getCasesDetails($id){
+        $where=['id'=>$id,'status'=>1];
+        $data = $this::with(['UserInfo'=>function($query){
+            $query->field('id,uid,nickname,headurl');
+        },'CasesImage'=>function($query){
+            $query->field('id,case_id,image');
+        }])
+            ->where($where)
+            ->find();
+
+        return empty($data)?[]:$data->toArray();
+    }
+
+    public function getDataByIDR($id,$uid){
+
+        $where = ['id'=>$id,'uid'=>$uid,'status'=>1];
+
+        $data = $this::with(['UserInfo'=>function($query){
+            $query->field('id,uid,nickname,headurl');
+        },'CasesFollow'=>function($query)use($uid){
+            $query->where(['uid'=>$uid])->field('id,touid,status');
+        },'CasesImage'=>function($query){
+            $query->field('id,case_id,image');
+        }])
+            ->where($where)
+            ->find();
+
+        return empty($data)?[]:$data->toArray();
+    }
+
+
+}
